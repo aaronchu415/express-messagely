d2340d47f68f271825bd5fb800e8cff0a7eac82d
aaronchu415 <aaronchu415@gmail.com>
2019-05-06 12:00:16 -0700

    added login route

--- config.js
+++ config.js
@@ -5,10 +5,13 @@
 require("dotenv").config();
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret";
+const JWT_SECRET = process.env.JWT_SECRET || "JWTsecret";
+
 const BCRYPT_WORK_ROUNDS = 10;
 
 
 module.exports = {
   SECRET_KEY,
+  JWT_SECRET,
   BCRYPT_WORK_ROUNDS,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,35 @@
+const express = require('express')
+const jwt = require("jsonwebtoken");
+const User = require('../models/user')
+const { JWT_SECRET } = require("../config")
+const ExpressError = require("../expressError")
+let router = new express.Router()
+
+const OPTIONS = { expiresIn: 60 * 60 }; // 1 hour
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
+
+router.post("/login", async function (req, res, next) {
+    try {
+        const { username, password } = req.body;
+
+        const USER_AUTHEN = await User.authenticate(username, password)
+
+        if (USER_AUTHEN) {
+            let token = jwt.sign({ username }, JWT_SECRET, OPTIONS);
+            return res.json({ token });
+        }
+        throw new ExpressError("Invalid user/password", 400);
+
+    } catch (err) {
+        return next(err);
+    }
+});
+
 
 
 /** POST /register - register user: registers, logs in, and returns token.
@@ -11,3 +38,5 @@
  *
  *  Make sure to update their last-login!
  */
+
+module.exports = router
